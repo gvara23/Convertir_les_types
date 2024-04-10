7218c637dba254432e0522805f36442cc7a472e3
Heidy <heidyguevara23@hotmail.com>
2024-04-10 08:55:02 +0200

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,24 @@
+﻿namespace Convertir_les_types
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the first number : ");
+            string inputFirst = Console.ReadLine();
+
+            Console.Write("Enter the second number : ");
+            string inputSecond = Console.ReadLine();
+
+            Console.Write("Enter a message of succes : ");
+            string successMessage = Console.ReadLine();
+
+            int numberOne = Convert.ToInt32(inputFirst);
+            int numberTwo = Convert.ToInt32(inputSecond);
+            int result = numberOne + numberTwo;
+
+            Console.WriteLine($"{numberOne} + {numberTwo} = {result}");
+            Console.WriteLine(successMessage);
+        }
+    }
+}
